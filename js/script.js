a5a2a17e668b84ec581d7e4ab3697d23114ce870
Roman Stelmakh <rstelmakh85@gmail.com>
2024-10-29 21:57:18 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// const productName = 'Droid';
+// console.log(productName);
+
+// const preicePerItem = 2000;
+// console.log(preicePerItem);
+// --------------------------------------
+// let pricePerItem = 2000;
+// pricePerItem = 3500;
+// console.log(pricePerItem);
+
+// let productName = 'Droid';
+// productName ='Repair droid';
+// console.log(productName);
+// ----------------------------------------
+// let topSpeed = 160;
+// console.log(typeof topSpeed);
+
+// let distance = 617.54;
+// console.log(typeof distance);
+
+// const login = 'mango935';
+// console.log(typeof 'mango935');
+
+// const isOnline = true;
+// console.log(typeof isOnline);
+
+// const isAdmin = false;
+// console.log(typeof isAdmin);
+// -------------------------------------
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+// ------------------------------------- 5
+
